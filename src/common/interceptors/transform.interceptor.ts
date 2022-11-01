ba03b46085c92b757fb95f0707de4bf608d34fea
dev-t2 <oo_ri@naver.com>
2022-11-01 09:50:07 +0900

    Auth POC Repository Reset

--- src/common/interceptors/transform.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { map } from 'rxjs';
-
-@Injectable()
-export class TransformInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {
-    return next.handle().pipe(map((data) => data ?? {}));
-  }
-}
